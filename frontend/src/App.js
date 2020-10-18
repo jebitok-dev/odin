fe1d18d9410ee77945156fd0ecff27fe58c6a46c
jebitok-dev <sharonkosgei4@gmail.com>
2020-10-18 23:04:25 +0300

    modify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,72 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component() {
+class RandomQuotes extends React.Component {
   constructor(props) {
     super(props)
 
   this.state = {
+    error: null,
+    isLoaded: false,
     quotes: [],
     author: ''
   }
 }
 
+componentDidMount() {
+  fetch("https://rapidapi.p.rapidapi.com/")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          quotes: result.quotes
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+}
+
 handleChange(e) {
   this.setState ({
-    input: e.target.value,
+    author: e.target.value,
     quotes: this.state.quotes
   })
 }  
 
 submitQuote() {
   this.setState ({
-    quotes: [...this.state.quotes, this.state.input],
+    quotes: [this.setState.quotes, this.state.author],
     author: ''
   })
 }
 
 
   render() {
-    return (
-      <div className="App" id="quote-box">
-        <h1 id="text">Education is the only equalizer</h1>
-        <p id="author">Nelson Mandela</p>
-        <input 
-    onChange={this.handleChange.bind(this)} value={this.state.input}
-  />
-        <button id="new-quote" onClick={this.submitQuote.bind(this)}><a>New Quote</a></button>
-      </div>
-    );
-  }
+    const { error, isLoaded, quotes, author } = this.state;
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
+      return (
+        <div className="App" id="quote-box">
+          <h1 id="text">Education is the only equalizer</h1>
+          <p id="author">Nelson Mandela</p>
+          <input 
+      onChange={this.handleChange.bind(this)} value={this.state.author}
+    />
+          <button id="new-quote" onClick={this.submitQuote.bind(this)}><a id="tweet-quote">New Quote</a></button>
+
+        </div>
+      );
+    }
+  // }
 }
 
-export default App;
+export default RandomQuotes;
